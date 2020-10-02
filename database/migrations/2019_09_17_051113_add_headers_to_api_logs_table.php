37552a54ae8e1fb7ded3ebc826c1b9ff2c548f11
Joris van Willigen <me@jorisvanw.me>
2020-10-02 08:53:24 +0200

    Add headers logging

---
+++ database/migrations/2019_09_17_051113_add_headers_to_api_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApiLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('api_logs', function (Blueprint $table) {
+
+            $table->text('headers')->nullable()->default(null)->after('url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('api_logs', function (Blueprint $table) {
+
+            $table->dropColumn('headers');
+
+        });
+    }
+}
