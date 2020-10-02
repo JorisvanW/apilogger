100a48206771eb4370490159fe0b2f37c94a72cd
Joris van Willigen <me@jorisvanw.me>
2020-10-02 09:26:50 +0200

    Added pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
 
     <title>{{ config('app.name', 'APILogger') }}</title>
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+{{--    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">--}}
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></head>
@@ -88,6 +88,10 @@
                       No Records
                     </h5>
                   @endforelse
+
+                    @if($apilogs instanceof \Illuminate\Pagination\LengthAwarePaginator )
+                        {{ $apilogs->links() }}
+                    @endif
 
                 </div>
             </div>
