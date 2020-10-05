3bc52fd961dc3d8c396aa70a8ae5d50f9a9c33a6
Joris van Willigen <me@jorisvanw.me>
2020-10-05 15:05:29 +0200

    Fix incorrect migration class name

--- database/migrations/2019_09_17_051113_add_headers_to_api_logs_table.php
+++ database/migrations/2019_09_17_051113_add_headers_to_api_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateApiLogsTable extends Migration
+class AddHeadersToApiLogsTable extends Migration
 {
     /**
      * Run the migrations.
